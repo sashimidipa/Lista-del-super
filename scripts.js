d478d9529794199e1155ea1f67ccbef2a4d4ba4b
Sashimidipa <sashaalexandradipaolo@gmail.com>
2023-02-11 15:30:52 -0300

    Finalizacion del proyecto (II)

--- scripts.js
+++ scripts.js
@@ -1,95 +1,3 @@
-// const smartself = []
-// let iva = 1.21
-// let ProductPrice = 0
-// let ProductName = ""
-// let ProductBrand = ""
-// let salirMenu = true
-// class Product {
-//     constructor(id, name, brand, price){
-//         this.id = id,
-//         this.name = ProductName,
-//         this.brand = ProductBrand,
-//         this.price = ProductPrice
-//     }
-// }
-// function addProduct (array) {
-//     let productName = prompt("Ingrese el nombre del Producto")
-//     ProductName = productName
-//     let productBrand = prompt('Ingrese la marca del Producto')
-//     ProductBrand = productBrand
-//     let productPrice = parseInt(prompt('Ingrese el precio del Producto'))
-//     if (!isNaN(productPrice)){
-//         ProductPrice = productPrice
-//     }
-//     else{ 
-//         alert('el precio ingresado debe ser un numero')
-//     }
-//     console.log (`${productName} ${productBrand} ${productPrice}`)
-
-//     const newProduct = new Product (array.length+1, productName, productBrand,productPrice)
-//     console.log(newProduct)
-//     array.push(newProduct)
-//     console.log(array)
-// }
-
-// function deleteProduct (array) {
-//     let prodString = [];
-//     let list = 'La lista de objetos es:';
-//         array.forEach(
-//             (prod)=>{
-//                 prodString.push(`\n ${prod.id} - ${prod.name} - ${prod.brand} que vale ${prod.price}`)
-//             }
-//         )
-//         for (let m in prodString){
-//             list = list + prodString[m];
-//         }
-//     let productSelection = prompt(list + '\n seleccione el producto a borrar');
-    
-    
-//     // let productSelection = prompt('Ingrese el nombre del producto que desea borrar')
-
-//     if ((productSelection) <= array.length){
-//         array.splice((productSelection -1), productSelection)
-//         alert ('el producto ingresado ha sido borrado')
-//     }
-//     else{
-//         alert ('el producto ingresado es inexistente')
-//     }
-// }
-
-// function calcTot (array){
-//     array.forEach(element => { console.log('el total de su compra es: ' + (sumar(element.price)))  
-//     })
-// }
-
-//  do {  
-
-//     let opcionMenu = prompt (`Ingrese la opción deseada
-//      1 - Agregar un producto
-//      2 - Borrar producto
-//      3 - Consultar precio final
-//      0 - Salir del ménu`)
-//      switch(opcionMenu){
-//          case "1":
-//             addProduct (smartself)
-//          break
-//         case "2":
-//             deleteProduct(smartself)
-//          break
-//          case "3":
-//             calcTot (smartself)
-//          break
-//          case "0":
-//              console.log("Salir del menu")
-//              salirMenu = false
-//          break
-//          default:
-//              console.log("Opción no válida")
-//          break             
-//     }
-// } while(salirMenu)
-
-//MUDANDO A DOM
 // ****** select items **********
 
 const form = document.querySelector(".grocery-form");
@@ -146,7 +54,18 @@ function addItem(e) {
     // append child
     list.appendChild(element);
     // display alert
-    displayAlert("El producto se agrego a la lista", "success");
+    Toastify({
+      text: "El producto se ha agregado con exito",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "center", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
     // show container
     container.classList.add("show-container");
     // set local storage
@@ -155,25 +74,47 @@ function addItem(e) {
     setBackToDefault();
   } else if (value !== "" && editFlag) {
     editElement.innerHTML = value;
-    displayAlert("Producto cambiado", "success");
+    Toastify({
+      text: "El producto se ha sido editado exitosamente",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "center", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
 
     // edit  local storage
     editLocalStorage(editID, value);
     setBackToDefault();
   } else {
-    displayAlert("Por favor ingrese un producto", "danger");
+    Toastify({
+      text: "Por favor ingrese un producto",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "center", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
   }
 }
-// display alert
-function displayAlert(text, action) {
-  alert.textContent = text;
-  alert.classList.add(`alert-${action}`);
-  // remove alert
-  setTimeout(function () {
-    alert.textContent = "";
-    alert.classList.remove(`alert-${action}`);
-  }, 1000);
-}
+// // display alert
+// function displayAlert(text, action) {
+//   alert.textContent = text;
+//   alert.classList.add(`alert-${action}`);
+//   // remove alert
+//   setTimeout(function () {
+//     alert.textContent = "";
+//     alert.classList.remove(`alert-${action}`);
+//   }, 1000);
+// }
 
 // clear items
 function clearItems() {
@@ -184,7 +125,18 @@ function clearItems() {
     });
   }
   container.classList.remove("show-container");
-  displayAlert("La lista ha sido vaciada", "danger");
+  Toastify({
+    text: "La lista ha sido vaciada",
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
   setBackToDefault();
   localStorage.removeItem("list");
 }
@@ -200,7 +152,18 @@ function deleteItem(e) {
   if (list.children.length === 0) {
     container.classList.remove("show-container");
   }
-  displayAlert("El producto ha sido eliminado", "danger");
+  Toastify({
+    text: "El producto ha sido eliminado",
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
 
   setBackToDefault();
   // remove from local storage
